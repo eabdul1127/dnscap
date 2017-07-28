3dd0def1071ff893e0904c3cdff94db9e39166f9
eabdul1127 <eabdul1127@gmail.com>
2017-07-27 23:33:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 echo "$0: install nodejs packages"
 npm install
 echo "$0: install and configure nginx"
-apt-get install nginx -y
-apt-get install apache2-utils -y
-mv ./default /etc/nginx/sites-available/default -f
-service nginx restart
+wget https://nginx.org/download/nginx-1.12.1.tar.gz
+tar xfvz nginx-1.12.1.tar.gz
+cd nginx-1.12.1/
+./configure --prefix=$HOME/nginx
+make -j 4
+make install
 echo "$0: update dns decode"
 mv ./dns.js ./node_modules/decode/dns.js -f
 echo "$0: Please run \"htpasswd /etc/nginx/.htpasswd USERNAME_HERE\" to create login credentials for the
