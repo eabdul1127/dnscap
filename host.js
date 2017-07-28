dbc6f7eb644da436c582673ab7064a9d4943f17d
eabdul1127 <eabdul1127@gmail.com>
2017-07-28 00:13:03 -0400

    Compress data,  group on interface for web display

--- host.js
+++ host.js
@@ -1,12 +1,12 @@
 var path = require("path");
-var zmq = require("zeromq"),
-    sock = zmq.socket("pull");
+var zmq = require("zeromq");
 var DNS = require("./node_modules/pcap/decode/dns.js"); // Local Copy of nodejs pcap modified for dns packet decoding to work properly
 var express = require("express");
 var os = require("os");
 var amqp = require("amqplib/callback_api");
 var async = require("async");
 var config = require("./config.js");
+var LZUTF8 = require('lzutf8');
 var SysLogger = require("ain2"),
     logger = new SysLogger();
 
@@ -14,7 +14,8 @@ var stats = {
   totalRequests: 0,
   recentRequests: 0,
   cpuUsage: 0,
-  freeMemory: 0
+  freeMemory: 0,
+  recent_interface: [0,0,0]
 };
 var packetSet = {};
 var currentInterval = 0;
@@ -30,16 +31,16 @@ app.get("/update", function (req, res) {
   stats.cpuUsage = (os.loadavg()[0]) / os.cpus().length;
   stats.freeMemory = os.freemem();
   res.json(stats);
-  stats.recentRequests=0;
+  stats.recentRequests = 0;
+  for(var i = 0; i < 3; i++)
+    stats.recent_interface[i] = 0;
 });
 
 app.listen(3000, "localhost", function () {
   console.log("Listening on port 3000!");
 });
 
-sock.bind("tcp://127.0.0.1:5000");
-
-sock.on("message", function(message) {
+var handleMessage = function (message) {
   var interval = Math.trunc(new Date().getTime() / config.intervalTimer) * config.intervalTimer;
   if(currentInterval != interval) {
     currentInterval = interval;
@@ -53,9 +54,13 @@ sock.on("message", function(message) {
     });
   }
   var finished_packet = interpretMessage(message);
-  if(finished_packet != undefined)
+  if(finished_packet != undefined) {
     addToPacketCounts(packetSet, finished_packet, 1);
-});
+    stats.recent_interface[this.interface_no]++;
+    stats.recentRequests++;
+    stats.totalRequests++;
+  }    
+};
 
 var elasticsearch_packet = function (host, status, origin, ips) {
   this.host = host;
@@ -71,8 +76,6 @@ var interpretMessage = function (message) {
   var parsedMessage = JSON.parse(message.toString());
   var buffer = new Buffer(parsedMessage.packetData);
   var decodedPacket = new DNS().decode(buffer, 0);
-  stats.recentRequests++;
-  stats.totalRequests++;
   var ips = [];
   try {
       answer_rrs = decodedPacket.answer.rrs;
@@ -126,7 +129,7 @@ var responseToString = function (responseCode) {
 };
 
 var cargo = async.cargo(function (data, cb) {
-  connectionChannel.sendToQueue("dnscap-q", new Buffer(JSON.stringify(data)));
+  connectionChannel.sendToQueue("dnscap-q", new Buffer.from(LZUTF8.compress(JSON.stringify(data))));
    return cb();
 }, config.CARGO_ASYNC);
 
@@ -135,3 +138,13 @@ amqp.connect("amqp://rabbitmqadmin:rabbitmqadmin@" + config.rabbit_master_ip_loc
     connectionChannel = ch;
   });
 });
+
+var sock = [];
+var enpoint = "tcp://127.0.0.1:";
+for(var i = 0; i < 3; i++)
+{
+  sock[i] = zmq.socket("pull");
+  sock[i].interface_no = (i);
+  sock[i].bind(enpoint + process.argv[i+2]);
+  sock[i].on("message", handleMessage);
+}
