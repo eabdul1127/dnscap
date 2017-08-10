08d1ed308d8e70029f9d1b853144f05dbf2931e1
eabdul1127 <eabdul1127@gmail.com>
2017-08-10 12:41:38 -0400

    Update dnscap.js

--- dnscap.js
+++ dnscap.js
@@ -14,14 +14,12 @@ var stats = {
   recentRequests: 0,
   cpuUsage: 0,
   freeMemory: 0,
-  recent_interface: [0,0,0],
-  total_interface: [0,0,0]
+  recent_interface: [],
+  total_interface: [],
+  errCount: 0
 };
 var packetSet = {};
-var currentInterval = 0;
-var connectionChannel;
 var app = express();
-var errCount = 0;
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/graph.html"));
@@ -32,7 +30,7 @@ app.get("/update", function (req, res) {
   stats.freeMemory = os.freemem();
   res.json(stats);
   stats.recentRequests = 0;
-  for(var i = 0; i < 3; i++)
+  for(var i = 0; i < process.argv.length-2; i++)
     stats.recent_interface[i] = 0;
 });
 
@@ -46,74 +44,15 @@ var addToPacketCounts = function (packetCounts, nextPacket, value) {
   packetCounts[key] = (packetCounts[key]+value) || value;
 };
 
-var interpretMessage = function (message) {
-  var parsedMessage = JSON.parse(message.toString());
-  var buffer = new Buffer(parsedMessage.packetData);
-  var decodedPacket = DNS.parse(buffer);
-  var ips = [];
-  answer_rrs = decodedPacket.answer;
-  question_rrs = decodedPacket.question;
-  var packetStatus = responseToString(decodedPacket.header.rcode);
-  var properResponse = false;
-  if(answer_rrs.length > 0) {
-    properResponse = answer_rrs.some(function (element, index, array) {
-      return element.type == 1;
-    });
-  }
-  if(!properResponse) {
-    if(question_rrs[0].type != 1 /* A record */ )
-    return;
-  }
-  for(var i = 0; i < answer_rrs.length; i++) {
-    if(answer_rrs[i].type == 1) {
-      ips.push(answer_rrs[i].address);
-    }
-  }
-  return new elasticsearch_packet(question_rrs[0].name, packetStatus, parsedMessage,  ips);
-}
-
-var handleMessage = function (message) {
-  var interval = Math.trunc(new Date().getTime() / config.intervalTimer) * config.intervalTimer;
-  if(currentInterval != interval) {
-    currentInterval = interval;
-    var setRef = packetSet;
-    packetSet = {};
-    Object.keys(setRef).forEach(function (message) {
-      var msg = {};
-      msg["date"] = interval;
-      msg[message] = setRef[message];
-      cargo.push(msg);
-    });
-  }
-  var finished_packet;
-  try {
-    finished_packet = interpretMessage(message);
-    if(finished_packet != undefined) {
-      addToPacketCounts(packetSet, finished_packet, 1);
-      stats.recent_interface[this.interface_no]++;
-      stats.recentRequests++;
-    }
-    else 
-      stats.totalFailedRequests++;
-  } catch(e) {
-    stats.totalFailedRequests++;
-    var errString = "Error Occurred: " + e + ", message: " + message ;
-    console.error(errString);
-  }
-  stats.totalRequests++;
-  stats.total_interface[this.interface_no]++;
-};
-
-var elasticsearch_packet = function (host, status, origin, ips) {
+var filtered_packet = function (host, status, origin, ips) {
   this.host = host;
   this.status = status;
   this.hashed_ip = origin.ip;
   this.hashed_mac = origin.mac;
   this.ips = ips;
   if(ips.length != 0)
-    this.ip = ips[0];
+  this.ip = ips[0];
 };
-
 var responseToString = function (responseCode) {
   try {
     return {
@@ -131,6 +70,68 @@ var responseToString = function (responseCode) {
   }
 };
 
+var interpretMessage = function (message) {
+  var parsedMessage = JSON.parse(message.toString());
+  var buffer = new Buffer(parsedMessage.packetData);
+  var decodedPacket = DNS.parse(buffer);
+  var ips = [];
+  answer_rrs = decodedPacket.answer;
+  question_rrs = decodedPacket.question;
+  var packetStatus = responseToString(decodedPacket.header.rcode);
+  var properResponse = false;
+  if(answer_rrs.length > 0) {
+    properResponse = answer_rrs.some(function (element, index, array) {
+      return element.type == 1;
+    });
+  }
+  if(!properResponse) {
+    return;
+  }
+  var ips = answer_rrs.map(function (record) {
+    if(record.type == 1)
+      return record.address;
+  });
+
+  return new filtered_packet(question_rrs[0].name, packetStatus, parsedMessage,  ips);
+}
+
+var currentInterval = Math.trunc(new Date().getTime() / config.intervalTimer) * config.intervalTimer;
+var handleMessage = function (message) {
+  var interval = Math.trunc(new Date().getTime() / config.intervalTimer) * config.intervalTimer;
+  if(currentInterval != interval) {
+    currentInterval = interval;
+    var setRef = packetSet;
+    packetSet = {};
+    Object.keys(setRef).forEach(function (message) {
+      var msg = {};
+      msg["date"] = interval;
+      msg[message] = setRef[message];
+      cargo.push(msg);
+    });
+  }
+  try {
+    var finished_packet = interpretMessage(message);
+    if(finished_packet == undefined)
+      throw new Error("Failed to decode message");
+    addToPacketCounts(packetSet, finished_packet, 1);
+    stats.recent_interface[this.interface_no]++;
+    stats.recentRequests++;
+  } catch(e) {
+    stats.totalFailedRequests++;
+    var errString = "Error Occurred: " + e + ", message: " + message ;
+    console.error(errString);
+  }
+  stats.totalRequests++;
+  stats.total_interface[this.interface_no]++;
+};
+
+amqp.connect(config.rabbit_master_ip, function (err, conn) {
+  conn.createChannel(function (err, ch) {
+    connectionChannel = ch;
+  });
+});
+
+var connectionChannel;
 var cargo = async.cargo(function (data, cb) {
   try{
     connectionChannel.sendToQueue("dnscap-q", new Buffer.from(LZUTF8.compress(JSON.stringify(data))));
@@ -139,11 +140,6 @@ var cargo = async.cargo(function (data, cb) {
   }
   return cb();
 }, config.CARGO_ASYNC);
-amqp.connect(config.rabbit_master_ip, function (err, conn) {
-  conn.createChannel(function (err, ch) {
-    connectionChannel = ch;
-  });
-});
 
 var enpoint = "tcp://127.0.0.1:";
 for(var i = 0; i < process.argv.length-2; i++) {
